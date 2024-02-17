945bf363b8eb2c620b89986018d027f4e0a71b10
PoPodada <goito.722@icloud.com>
2024-02-18 06:39:24 +0900

    共通部分のヘッダーロゴにトップページリンクを実装

--- front/src/components/layout.tsx
+++ front/src/components/layout.tsx
@@ -1,4 +1,4 @@
-import {Outlet} from "react-router-dom";
+import {Outlet,Link} from "react-router-dom";
 import progateLogo from "@/assets/progate_logo.svg";
 import helpfeelLogo from "@/assets/helpfeel_logo.svg";
 import {ImCross} from "react-icons/im";
@@ -7,9 +7,11 @@ export function Layout(): JSX.Element {
   return (
     <div>
       <header className="flex h-8 m-4 items-center">
+        <Link to={""} className="flex h-8 m-4 items-center">
         <img src={progateLogo} alt="progate logo" className="h-full" />
         <ImCross className="h-3 mx-2" />
         <img src={helpfeelLogo} alt="helpfeel logo" className="h-full" />
+        </Link>
       </header>
       <main className="bg-[#F9FBFE] h-[calc(100vh-8rem)] py-8 md:py-14 px-4 flex justify-center">
         <div className="w-full md:max-w-[760px]">
