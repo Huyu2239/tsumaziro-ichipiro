b0bc08e34290debf1abe657621558897fbba162a
Uma821 <uma.ohmoto@gmail.com>
2024-02-18 14:13:55 +0900

    ゲーム背景画像を画面サイズに応じて変更

--- front/src/pages/game.tsx
+++ front/src/pages/game.tsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react';
 import ninja from '../images/ninja.png';
 import place from '../images/place.jpg';
+import place_mini1 from '../images/place_mini1.jpg';
+import place_mini2 from '../images/place_mini2.jpg';
 import { useStopwatch } from "react-timer-hook";
 import '../game.css'
 export function GamePage(): JSX.Element {
   const [clickCount, setClickCount] = useState(0);
-  const { seconds, minutes, isRunning, start, pause, reset } =useStopwatch({ autoStart: false });
+  const {seconds, minutes, isRunning, start, pause, reset} = useStopwatch({ autoStart: false });
+  const [size, setSize] = useState({ 'width': window.innerWidth, 'height': window.innerHeight });
+  window.addEventListener('resize', function () {
+    setSize({ 'width': window.innerWidth, 'height': window.innerHeight });
+  }, true);
  
   useEffect(() => {
     if (clickCount === 5) {
@@ -13,7 +19,7 @@ export function GamePage(): JSX.Element {
     } else {
       placeHiddenPhoto(); // それ以外は画像を配置
     }
-    }, [clickCount, pause, seconds, minutes, isRunning, start, reset]);
+  }, [clickCount, pause, seconds, minutes, isRunning, start, reset]);
 
   const placeHiddenPhoto = () => {
     const maxX = window.innerWidth - 150;
@@ -69,7 +75,7 @@ export function GamePage(): JSX.Element {
         </h3>}
       </div>
       <main className="main-background">
-        <img src={place}/>
+        <div style={{width: "100vw", marginLeft:"calc(-50vw + 50%)"}}><img style={{width:"100%"}} src={size.width > 400 * 2 ? size.width > 500 * 2 ? place_mini2 : place_mini1 : place}/></div>
         <div className="hidden-photo"
         style={{ ...photoPosition, display: hidden ? 'block' : 'none' }}
         onClick={foundPhoto} > 
